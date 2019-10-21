202416e2e46c11329aa3aa92a49fe8fbec14e879
Elie <milhouze@live.fr>
2019-10-21 21:55:32 +0200

    Creation of the Project. MVC Model. Creation of a first version of a responsive storyboard for iPhone portait and landscape.

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Elie Arquier on 15/10/2019.
+//  Copyright © 2019 Elie Arquier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
