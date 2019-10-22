e64c4fb2c7fc84bcb2e2ad5f08b0c807751cc439
Elie <milhouze@live.fr>
2019-10-22 23:35:54 +0200

    Creation of a custom UIView class : GridView. Implementation of selected buttons and grids in ViewController.

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gridView: GridView!
+    @IBOutlet var bottomButtons: [UIButton]!
+    @IBOutlet var selectedBottomButtons: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenFirstButtonIsTapped(_ sender: UIButton) {
+        selectFirstDisposition()
+    }
+    
+    @IBAction func whenCenterButtonIsTapped(_ sender: UIButton) {
+        selectCenterDisposition()
+    }
+    
+    @IBAction func whenLastButtonIsTapped(_ sender: UIButton) {
+        selectLastDisposition()
+    }
+    
+    func selectFirstDisposition() {
+        gridView.grid = .first
+        untapButtons()
+        selectedBottomButtons[0].isHidden = false
+    }
+    
+    func selectCenterDisposition() {
+        gridView.grid = .center
+        untapButtons()
+        selectedBottomButtons[1].isHidden = false
+    }
+    
+    func selectLastDisposition() {
+        gridView.grid = .last
+        untapButtons()
+        selectedBottomButtons[2].isHidden = false
+    }
+    
+    func untapButtons() {
+        for index in 0 ... selectedBottomButtons.count - 1 {
+            selectedBottomButtons[index].isHidden = true
+        }
+    }
 }
 
---
+++ Instagrid/View/GridView.swift
@@ -0,0 +1,55 @@
+//
+//  GridView.swift
+//  Instagrid
+//
+//  Created by Elie Arquier on 22/10/2019.
+//  Copyright © 2019 Elie Arquier. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+
+    enum Grid {
+        case first, center, last
+    }
+    
+    var grid: Grid = .center {
+        didSet {
+            switch grid {
+            case .first:
+                first()
+            case .center:
+                center()
+            case .last:
+                last()
+            }
+        }
+    }
+    
+    func first() {
+        topLeftButton.isHidden = false
+        topRightButton.isHidden = true
+        bottomLeftButton.isHidden = false
+        bottomRightButton.isHidden = false
+    }
+    
+    func center() {
+        topLeftButton.isHidden = false
+        topRightButton.isHidden = false
+        bottomLeftButton.isHidden = false
+        bottomRightButton.isHidden = true
+    }
+    
+    func last() {
+        topLeftButton.isHidden = false
+        topRightButton.isHidden = false
+        bottomLeftButton.isHidden = false
+        bottomRightButton.isHidden = false
+    }
+    
+    @IBOutlet var topLeftButton: UIButton!
+    @IBOutlet var topRightButton: UIButton!
+    @IBOutlet var bottomLeftButton: UIButton!
+    @IBOutlet var bottomRightButton: UIButton!
+}
